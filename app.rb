d8e95d518e05234e5fbf1173c9eea0e363cd670c
PoulTch <pavelchar@gmail.com>
2024-12-17 14:18:06 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -9,9 +9,16 @@ def init_db
 	@db.results_as_hash = true
 end
 
+# before вызывается каждый раз при перезагрузке 
+# любой страницы
+
 before do
+	# инициализация БД
 	init_db
 end
+
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница
 
 configure do
 	init_db
@@ -27,11 +34,18 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработчик get-запроса /new
+# (браузер) получает страницу с сервера
+
 get '/new' do
   erb :new
 end
 
+# обработчик post-запроса /new
+# (браузер) отправляет данные на сервер
+
 post '/new' do
+  # получает переменную из post-запроса
   content = params[:content]
   erb "You typed #{content}"
 end
